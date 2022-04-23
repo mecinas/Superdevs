6de0dc728e0d63c37f4ea16172071742ac5b37af
mecinas <szymon.mecina@vp.pl>
2022-04-23 15:41:38 +0200

    Adding heroku app

--- src/main/java/com/warehouse/superdevs/config/SwaggerConfig.java
+++ src/main/java/com/warehouse/superdevs/config/SwaggerConfig.java
@@ -14,7 +14,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.ArrayList;
 
 @EnableSwagger2
-@Configuration
 public class SwaggerConfig {
     @Bean
     public Docket api() {
