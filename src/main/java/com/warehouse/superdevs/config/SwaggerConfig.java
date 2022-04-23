4f0011d737344bec83b8391da74c2e264563455a
mecinas <szymon.mecina@vp.pl>
2022-04-23 15:37:07 +0200

    Adding heroku app

--- src/main/java/com/warehouse/superdevs/config/SwaggerConfig.java
+++ src/main/java/com/warehouse/superdevs/config/SwaggerConfig.java
@@ -2,21 +2,37 @@ package com.warehouse.superdevs.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static springfox.documentation.builders.PathSelectors.regex;
+import java.util.ArrayList;
 
 @EnableSwagger2
+@Configuration
 public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.waregouse.superdevs.controller"))
-                .paths(regex("/api.*"))
-                .build();
+                .paths(PathSelectors.ant("/api.*"))
+                .build()
+                .apiInfo(metaData());
+    }
+    private ApiInfo metaData() {
+        return new ApiInfo(
+                "MS Posts : Spring Boot REST API",
+                "Spring Boot REST API for Posts management",
+                "1.0",
+                "Terms of service",
+                new Contact("Azhwani", "https://devwithus.com", "azhwani@devwithus.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0", new ArrayList<VendorExtension>());
     }
 }
